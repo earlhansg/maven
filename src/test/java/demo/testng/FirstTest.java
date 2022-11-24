f342e482b4d8719ddbbde3ee18166a1eb88de9a1
Earl Hans Genoso <earlhansg@gmail.com>
2022-11-25 01:00:06 +0800

    annotations part two

--- src/test/java/demo/testng/FirstTest.java
+++ src/test/java/demo/testng/FirstTest.java
@@ -1,8 +1,13 @@
 package demo.testng;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
@@ -14,13 +19,34 @@ public class FirstTest {
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\EarlHans Geñoso\\Desktop\\Resources\\driver_v1\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.manage().window().maximize();	
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		Thread.sleep(2000);
+	}
+	
+	@BeforeMethod
+	public void signin() throws InterruptedException {
+		driver.get("http://teststore.automationtesting.co.uk/");
+		driver.findElement(By.cssSelector("[title] .hidden-sm-down")).click();
+		driver.findElement(By.cssSelector("section input[name='email']")).sendKeys("test@test.com");
+		driver.findElement(By.cssSelector("input[name='password']")).sendKeys("test123");
+		driver.findElement(By.cssSelector("button#submit-login")).click();
+		System.out.println("user has logged in");
 		Thread.sleep(2000);
 	}
 
 	@Test
 	public void test() throws InterruptedException {
-		driver.get("https://www.automationtesting.co.uk");
+		driver.findElement(By.linkText("CLOTHES")).click();
+		System.out.println("user has clicked the clothes link");
+		Thread.sleep(2000);
+	}
+	
+	@AfterMethod
+	public void signout() throws InterruptedException {
+		driver.findElement(By.cssSelector(".hidden-sm-down.logout")).click();
+		System.out.println("user has logged out");
 		Thread.sleep(2000);
 	}
 

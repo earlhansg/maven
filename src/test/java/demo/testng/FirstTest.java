ab8020bcabf299c7ab53ae442575bfaff821c0d7
Earl Hans Genoso <earlhansg@gmail.com>
2022-11-24 23:46:57 +0800

    annotations part one

--- src/test/java/demo/testng/FirstTest.java
+++ src/test/java/demo/testng/FirstTest.java
@@ -1,11 +1,32 @@
 package demo.testng;
 
-import org.testng.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class FirstTest {
-    @Test
-	public void testng() {
-		Assert.assertEquals("demo", "demo");
+    WebDriver driver;
+
+	@BeforeSuite
+	public void setup() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\EarlHans Geñoso\\Desktop\\Resources\\driver_v1\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();	
+		Thread.sleep(2000);
+	}
+
+	@Test
+	public void test() throws InterruptedException {
+		driver.get("https://www.automationtesting.co.uk");
+		Thread.sleep(2000);
+	}
+
+	@AfterSuite
+	public void end() {
+		driver.close();
+		driver.quit();
 	}
 }

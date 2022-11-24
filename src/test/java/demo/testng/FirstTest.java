d0c7fad11b1989cc66792ea5384aafb04df6fcc1
Earl Hans Genoso <earlhansg@gmail.com>
2022-11-24 22:55:45 +0800

    executing methods by common name

--- src/test/java/demo/testng/FirstTest.java
+++ src/test/java/demo/testng/FirstTest.java
@@ -1,11 +1,30 @@
 package demo.testng;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FirstTest {
     @Test
-	public void testng() {
-		Assert.assertEquals("demo", "demo");
+	public void Desktop_login() {
+		System.out.println("Desktop login");
+	}
+	@Test
+	public void Desktop_addItem() {
+		System.out.println("Desktop Add Item");
+	}
+	@Test
+	public void Tablet_login() {
+		System.out.println("Tablet login");
+	}
+	@Test
+	public void Tablet_addItem() {
+		System.out.println("Tablet Add Item");
+	}
+	@Test
+	public void Mobile_login() {
+		System.out.println("Mobile login");
+	}
+	@Test
+	public void Mobile_addItem() {
+		System.out.println("Mobile Add Item");
 	}
 }

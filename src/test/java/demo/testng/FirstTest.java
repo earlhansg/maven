1583b4cf212d2edba57440bed6fe44e29f34f911
Earl Hans Genoso <earlhansg@gmail.com>
2022-12-01 22:54:23 +0800

    running test with dependsOnMethods

--- src/test/java/demo/testng/FirstTest.java
+++ src/test/java/demo/testng/FirstTest.java
@@ -1,11 +1,41 @@
 package demo.testng;
 
-import org.testng.Assert;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class FirstTest {
-    @Test
-	public void testng() {
-		Assert.assertEquals("demo", "demo");
+    WebDriver driver;
+
+	@BeforeSuite
+	public void setup() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\EarlHans Geñoso\\Desktop\\Resources\\driver_v1\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://teststore.automationtesting.co.uk/login?back=my-account");
+		Thread.sleep(3000);
+	}
+
+	@Test
+	public void enterEmail() throws InterruptedException {
+		driver.findElement(By.cssSelector("section input[name='email']")).sendKeys("test@test.com");
+		Thread.sleep(3000);
+	}
+
+	@Test
+	public void enterPassword() throws InterruptedException {
+		driver.findElement(By.cssSelector("input[name='password']")).sendKeys("test123");
+		Thread.sleep(3000);
+	}
+
+	@Test(dependsOnMethods = { "enterPassword" })
+	public void clickBtn() throws InterruptedException {
+		driver.findElement(By.cssSelector("button#submit-login")).click();
 	}
 }

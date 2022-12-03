096ba5ccebd1b3085931fd8d0f3ef4145fd65b86
Earl Hans Genoso <earlhansg@gmail.com>
2022-12-04 00:05:34 +0800

    Creating Base Class to avoid redundancy

---
+++ src/test/java/demo/testng/BasePage.java
@@ -0,0 +1,70 @@
+package demo.testng;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BasePage {
+    public static WebDriver driver;
+	private String url;
+
+	public WebDriver getDriver() throws IOException {
+		Properties prop = new Properties();
+		FileInputStream data = new FileInputStream(
+				"C:\\Users\\EarlHans Geñoso\\Desktop\\Resources\\VSCode\\maven\\src\\test\\java\\demo\\testng\\config.properties");
+		prop.load(data);
+
+		if (prop.getProperty("browser").equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\EarlHans Geñoso\\Desktop\\Resources\\driver_v1\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (prop.getProperty("browser").equals("firefox")) {
+			System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\EarlHans Geñoso\\Desktop\\Resources\\driver_v1\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else {
+			System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\EarlHans Geñoso\\Desktop\\Resources\\driver_v1\\msedgedriver.exe");
+			driver = new EdgeDriver();
+		}
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		return driver;
+	}
+
+	public String getUrl() throws IOException {
+		Properties prop = new Properties();
+		FileInputStream data = new FileInputStream(
+				"C:\\Users\\EarlHans Geñoso\\Desktop\\Resources\\VSCode\\maven\\src\\test\\java\\demo\\testng\\config.properties");
+		prop.load(data);
+		url = prop.getProperty("url");
+		return url;
+	}
+
+	public void takeSnapShot(WebDriver webdriver) throws IOException {
+		File srcFile = ((TakesScreenshot) webdriver).getScreenshotAs(OutputType.FILE);
+
+		File destFile = new File("C:\\Users\\EarlHans Geñoso\\Desktop\\Resources\\screenshots"
+				+ timestamp() + ".png");
+
+		FileUtils.copyFile(srcFile, destFile);
+	}
+
+	public String timestamp() {
+		return new SimpleDateFormat("yyyy-MM-dd HH-mm-ss").format(new Date());
+	}
+}
--- src/test/java/demo/testng/FirstTest.java
+++ src/test/java/demo/testng/FirstTest.java
@@ -1,51 +1,17 @@
 package demo.testng;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-public class FirstTest {
-	WebDriver driver;
-
-	@BeforeSuite
-	public void setup() throws InterruptedException, IOException {
-		Properties prop = new Properties();
-		FileInputStream data = new FileInputStream(
-				"C:\\Users\\EarlHans Geñoso\\Desktop\\Resources\\VSCode\\maven\\src\\test\\java\\demo\\testng\\config.properties");
-		prop.load(data);
-
-		if (prop.getProperty("browser").equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\EarlHans Geñoso\\Desktop\\Resources\\driver_v1\\chromedriver.exe");
-			driver = new ChromeDriver();
-		} else if (prop.getProperty("browser").equals("firefox")) {
-			System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\EarlHans Geñoso\\Desktop\\Resources\\driver_v1\\geckodriver.exe");
-			driver = new FirefoxDriver();
-		} else {
-			System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\EarlHans Geñoso\\Desktop\\Resources\\driver_v1\\msedgedriver.exe");
-			driver = new EdgeDriver();
-		}
-
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		Thread.sleep(2000);
-
-		driver.get(prop.getProperty("url"));
-	}
+public class FirstTest extends BasePage {
 
 	@Test
-	public void signin() throws InterruptedException {
+	public void signin() throws InterruptedException, IOException {
+
+		driver = getDriver();
+		driver.get(getUrl());
 
 		driver.findElement(By.cssSelector("[title] .hidden-sm-down")).click();
 		driver.findElement(By.cssSelector("section input[name='email']")).sendKeys("test@test.com");

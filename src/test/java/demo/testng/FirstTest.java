9f5d98a43f9ada6f155b50ddb75fbe589729eae8
Earl Hans Genoso <earlhansg@gmail.com>
2022-12-03 22:29:06 +0800

    Parameterising global variables

--- src/test/java/demo/testng/FirstTest.java
+++ src/test/java/demo/testng/FirstTest.java
@@ -1,11 +1,44 @@
 package demo.testng;
 
-import org.testng.Assert;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class FirstTest {
-    @Test
-	public void testng() {
-		Assert.assertEquals("demo", "demo");
+	WebDriver driver;
+
+	@BeforeSuite
+	public void setup() throws InterruptedException, IOException {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\EarlHans Geñoso\\Desktop\\Resources\\driver_v1\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		Thread.sleep(2000);
+
+		Properties prop = new Properties();
+		FileInputStream data = new FileInputStream(
+				"C:\\Users\\EarlHans Geñoso\\Desktop\\Resources\\VSCode\\maven\\src\\test\\java\\demo\\testng\\config.properties");
+		prop.load(data);
+
+		driver.get(prop.getProperty("url"));
+	}
+
+	@Test
+	public void signin() throws InterruptedException {
+
+		driver.findElement(By.cssSelector("[title] .hidden-sm-down")).click();
+		driver.findElement(By.cssSelector("section input[name='email']")).sendKeys("test@test.com");
+		driver.findElement(By.cssSelector("input[name='password']")).sendKeys("test123");
+		driver.findElement(By.cssSelector("button#submit-login")).click();
+		System.out.println("user has logged in");
+		Thread.sleep(2000);
 	}
 }
